e52b076572240297dc9c8b9e126e021d846918b3
simpatine <simpatine@gmail.com>
2025-08-01 12:24:41 +0200

    GNN eval

--- src/eval.rs
+++ src/eval.rs
@@ -30,7 +30,7 @@ impl Board {
         my_score - their_score
     }
 
-    pub fn GNN_eval(&mut self) -> Eval {
+    pub fn gnn_eval(&mut self) -> Eval {
         let graph = self.get_graph();
         self.gnn.evaluate(&graph).unwrap_or(0.0) as Eval
     }
